87f0e8daaa75475736e894d3f68153a10490ccae
1dilia <returnidilia@gmail.com>
2018-03-22 18:28:45 +0300

    minor changes

--- src/tests/Map.java
+++ src/tests/Map.java
@@ -25,9 +25,8 @@ public class Map {
 	}
 
 	@Test
-	public void testMap() throws Exception {
-		 driver.get(Propirties.getBaseUrl()); //без регистрации
-//		Helper.logIn(Propirties.getLogin(), Propirties.getPassword(), driver);
+	public void testMapWithoutRegistration() throws Exception {
+		driver.get(Propirties.getBaseUrl()); 
 		driver.findElement(Locators.getButtonContact()).click();
 		driver.findElement(Locators.getFieldMapAddress()).click();
 		driver.findElement(Locators.getFieldMapAddress()).click();
@@ -45,7 +44,29 @@ public class Map {
 		driver.findElement(Locators.getButtonFull()).click();
 		driver.findElement(Locators.getButtonFullBack()).click();
 		driver.findElement(Locators.getButtonHomePage()).click();
-//		driver.findElement(Locators.getButtonLogOut()).click();
+	}
+	
+	@Test
+	public void testMapWithRegistration() throws Exception {
+		Helper.logIn(Propirties.getLogin(), Propirties.getPassword(), driver);
+		driver.findElement(Locators.getButtonContact()).click();
+		driver.findElement(Locators.getFieldMapAddress()).click();
+		driver.findElement(Locators.getFieldMapAddress()).click();
+		driver.findElement(Locators.getFieldMapAddress()).clear();
+		driver.findElement(Locators.getFieldMapAddress()).sendKeys(Propirties.getAddressMap());
+		driver.findElement(Locators.getButtonMapSearch()).click();
+		assertTrue(isElementPresent(Locators.getTitlePopUp()));
+		driver.findElement(Locators.getClosePopUp()).click();
+		driver.findElement(Locators.getButtonZoomIn()).click();
+		driver.findElement(Locators.getButtonZoomOut()).click();
+		driver.findElement(Locators.getButtonList()).click();
+		driver.findElement(Locators.getButtonListSatellite()).click();
+		driver.findElement(Locators.getButtonList()).click();
+		driver.findElement(Locators.getButtonListHybrid()).click();
+		driver.findElement(Locators.getButtonFull()).click();
+		driver.findElement(Locators.getButtonFullBack()).click();
+		driver.findElement(Locators.getButtonHomePage()).click();
+		driver.findElement(Locators.getButtonLogOut()).click();
 	}
 
 	@AfterClass(alwaysRun = true)
--- src/tests/Register.java
+++ src/tests/Register.java
@@ -3,13 +3,10 @@ package tests;
 import java.util.regex.Pattern;
 import java.util.concurrent.TimeUnit;
 import org.testng.annotations.*;
-
-import classRetired.DataDriven2;
 import helper.DataDriven;
 import helper.Helper;
 import helper.Locators;
 import helper.Propirties;
-
 import static org.testng.Assert.*;
 import org.openqa.selenium.*;
 import org.openqa.selenium.firefox.FirefoxDriver;
@@ -82,112 +79,16 @@ public class Register {
 			} catch (Error e) {
 				verificationErrors.append(e.toString());
 			}
-			// driver.manage().timeouts().implicitlyWait(70, TimeUnit.SECONDS);
 			driver.findElement(Locators.getButtonLogOut()).click();
 			break;
-		 case "4":
-		 assertEquals(driver.findElement(By.cssSelector(".panel-heading")).getText(),
-		 "Register");
-		 break;
-		 case "5":
-		 assertEquals(driver.findElement(By.cssSelector(".panel-heading")).getText(),
-		 "Register");
-		 break;
+		case "4":
+			assertEquals(driver.findElement(By.cssSelector(".panel-heading")).getText(), "Register");
+			break;
+		case "5":
+			assertEquals(driver.findElement(By.cssSelector(".panel-heading")).getText(), "Register");
+			break;
 		}
 	}
-
-	// @Test(dataProvider = "dataRegister", dataProviderClass = DataDriven.class)
-	// public void testRegistrationForm(String name, String mail, String password,
-	// String passwordConfirm, String result) throws Exception {
-	//
-	// Helper.makeRegister(name, mail, password, passwordConfirm, driver);
-	//
-	// switch (result) {
-	// case "1":
-	// try {
-	// assertTrue(isElementPresent(Locators.getButtonLogOut()));
-	// } catch (Error e) {
-	// verificationErrors.append(e.toString());
-	// }
-	// driver.findElement(Locators.getButtonLogOut()).click();
-	// break;
-	// case "2":
-	// assertEquals(driver.findElement(By.cssSelector(".panel-heading")).getText(),
-	// "Register");
-	// break;
-	// case "3":
-	// assertEquals(driver.findElement(By.cssSelector(".panel-heading")).getText(),
-	// "Register");
-	// break;
-	// case "4":
-	// assertEquals(driver.findElement(By.cssSelector(".panel-heading")).getText(),
-	// "Register");
-	// break;
-	// case "5":
-	// assertEquals(driver.findElement(By.cssSelector(".panel-heading")).getText(),
-	// "Register");
-	// break;
-	// }
-	// }
-	//
-	// @Test(dataProvider = "dataRegisterName", dataProviderClass =
-	// DataDriven.class)
-	// public void testRegistrationName(String name, String mail, String password,
-	// String passwordConfirm, String result) throws Exception {
-	//
-	// Helper.makeRegister(name, mail, password, passwordConfirm, driver);
-	//
-	// switch (result) {
-	// case "1":
-	// try {
-	// assertTrue(isElementPresent(Locators.getButtonLogOut()));
-	// } catch (Error e) {
-	// verificationErrors.append(e.toString());
-	// }
-	// driver.findElement(Locators.getButtonLogOut()).click();
-	// break;
-	// case "2":
-	// try {
-	// assertTrue(isElementPresent(Locators.getButtonLogOut()));
-	// } catch (Error e) {
-	// verificationErrors.append(e.toString());
-	// }
-	// driver.findElement(Locators.getButtonLogOut()).click();
-	//
-	//
-	// case "3":
-	// assertEquals(driver.findElement(By.cssSelector(".panel-heading")).getText(),
-	// "Register");
-	// break;
-	// case "4":
-	// assertEquals(driver.findElement(By.cssSelector(".panel-heading")).getText(),
-	// "Register");
-	// break;
-	// case "5":
-	// assertEquals(driver.findElement(By.cssSelector(".panel-heading")).getText(),
-	// "Register");
-	// break;
-	// }
-	// }
-
-	//
-	// @Test(dataProvider = "dataRegister", dataProviderClass = DataDriven.class)
-	// public void testRegisterFieldEmail(String name, String login, String
-	// password, String result) throws Exception {
-	// driver.get(Propirties.getBaseUrl());
-	// driver.findElement(Locators.getButtonRegister()).click();
-	// driver.findElement(Locators.getFieldNameReg()).clear();
-	// driver.findElement(Locators.getFieldNameReg()).sendKeys("1");
-	// driver.findElement(Locators.getFieldEmailReg()).clear();
-	// driver.findElement(Locators.getFieldEmailReg()).sendKeys("dijn11@gfg.ru");
-	// driver.findElement(Locators.getFieldPasswordReg()).clear();
-	// driver.findElement(Locators.getFieldPasswordReg()).sendKeys("111111");
-	// driver.findElement(Locators.getFieldPasswordConfirmReg()).clear();
-	// driver.findElement(Locators.getFieldPasswordConfirmReg()).sendKeys("111111");
-	// driver.findElement(Locators.getButtonAcceptRegister()).click();
-	// driver.findElement(Locators.getButtonLogOut()).click();
-	//
-	// }
 
 	@AfterClass(alwaysRun = true)
 	public void tearDown() throws Exception {
